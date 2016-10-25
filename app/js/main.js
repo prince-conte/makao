654ad46f4710c3f3a76d3f69f9c30ddd0723f89a
prince-conte <prince-conte@outlook.com>
2016-10-26 00:19:14 +0300

    yap

---
+++ app/js/main.js
@@ -0,0 +1,16 @@
+ $(document).ready(function() {
+
+     
+     
+$('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1
+});
+     
+     
+     
+     
+     
+});
+ 
